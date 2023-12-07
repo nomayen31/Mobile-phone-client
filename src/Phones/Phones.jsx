dc9a1e4c54a722670e9a32f7c9e944683b3b229a
nomayen <nomayen.ohin@gmail.com>
2023-12-08 01:16:07 +0600

    first commit

---
+++ src/Phones/Phones.jsx
@@ -0,0 +1,16 @@
+import { Link, useLoaderData } from 'react-router-dom';
+
+const Phones = () => {
+    const phones = useLoaderData()
+    console.log(phones);
+    return (
+        <div>
+            <h1>All Phones Here: {phones.length} </h1>
+            {
+                phones.map(phone => <li key={phone.id}><Link to={`/phone/${phone.id}`}>{phone.name}</Link></li>)
+            }
+        </div>
+    );
+};
+
+export default Phones;
